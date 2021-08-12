fc4fde18d2e8daed4eb9ecba9c996baa2cfe94e3
Andrei Damian <andreidaamian@gmail.com>
2021-08-13 02:07:10 +0300

    trying lalrpop

--- tom/tom_lang/tom_lang/src/main.rs
+++ tom/tom_lang/tom_lang/src/main.rs
@@ -1,22 +1,10 @@
-use llvm_free::{Module, Function};
-use pest_derive::Parser;
-use pest::Parser;
+mod untyped_ast;
 
-#[derive(Parser)]
-#[grammar = "tom.pest"]
-pub struct JsonParser;
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(pub jerry); // synthesized by LALRPOP
 
 fn main() {
-    // let s = "{\"a\" : [1, 2, 3]}";
-    // println!("{:?}", JsonParser::parse(Rule::json, s));
-
-    let module = Module::new("branza".into());
-    let function = module.function(Some("hello".into()));
-
-    let bb = function.add_basic_block(Some("entry".into()));
-    let builder = bb.builder();
-    let type_int32 = builder.type_int(32);
-    let const_int32 = builder.const_int(53, type_int32);
-
-    println!("{:?}", const_int32);
+    let x = jerry::ItemParser::new().parse("fn aaa {}");
+    println!("{:?}", x);
 }
---
+++ tom/tom_lang/tom_lang/src/untyped_ast.rs
@@ -0,0 +1,22 @@
+#[derive(Debug)]
+pub struct Location {
+    pub start: u32,
+    pub end: u32,
+}
+
+impl Location {
+    pub(crate) fn new(start: usize, end: usize) -> Location {
+        Location { start: start as u32, end: end as u32 }
+    }
+}
+
+#[derive(Debug)]
+pub enum Item {
+    Function(Function)
+}
+
+#[derive(Debug)]
+pub struct Function {
+    pub loc: Location,
+    pub name: String
+}
